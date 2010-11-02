e9a042ef6541d727623994ec678a1ec15650af86
stevesoudersorg <stevesoudersorg@gmail.com>
2010-11-02 02:57:47 +0000

    sort episodes by start time first and length second

--- episodes.js
+++ episodes.js
@@ -389,7 +389,13 @@ EPISODES.drawEpisodes = function(parent, bMarks) {
 
 EPISODES.sortEpisodes = function(a, b) {
 	if ( a[0] == b[0] ) {
-		return 0;
+		if ( a[1] == b[1] ) {
+			return 0;
+		}
+		if ( a[1] > b[1] ) {
+			return -1;
+		}
+		return 1;
 	}
 	if ( a[0] < b[0] ) {
 		return -1;
