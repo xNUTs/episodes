3d349e2f8b5aaa1fb31188c0aedfb626fd034362
stevesoudersorg@gmail.com <stevesoudersorg@gmail.com@2ef5004e-2e3e-11de-bd8d-4b29057232bf>
2014-09-04 04:04:50 +0000

    clearer

--- trunk/index.php
+++ trunk/index.php
@@ -10,26 +10,11 @@ EPISODES.q = []; // command queue
 EPISODES.mark = function(mn, mt) { EPISODES.q.push( ["mark", mn, mt || new Date().getTime()] ); };
 EPISODES.measure = function(en, st, en) { EPISODES.q.push( ["measure", en, st, en || new Date().getTime()] ); };
 EPISODES.done = function(callback) { EPISODES.q.push( ["done", callback] ); };
+EPISODES.bResourceTimingAgg = true;   // get aggregate Resource Timing stats (optional)
 EPISODES.mark("firstbyte");
-(function() {
-    var epjs = document.createElement("script"); epjs.type = "text/javascript"; epjs.async = true;
-    epjs.src = "episodes.js";
-    var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(epjs, s);
-})();
 </script>
+<script async defer src="episodes.js"></script>
 
-<script>
-function installXPI(href)
-{
-    if (typeof(InstallTrigger) != 'undefined') {
-        var xpi = {'episodes': href};
-        InstallTrigger.install(xpi);
-    }
-    else {
-        alert("You must be using Firefox.");
-    }
-}
-</script>
 
 <style>
 BODY { font-family: arial; width: 900px; }
@@ -38,36 +23,31 @@ BODY { font-family: arial; width: 900px; }
 <body>
 
 <div style="float: right; font-size: 0.9em;">
-<nobr>
-<a href="http://groups.google.com/group/webepisodes/topics">contact us</a>
-&nbsp;|&nbsp;
-<a href="http://code.google.com/p/episodes/source/checkout">code</a>
-&nbsp;|&nbsp;
-<a href="http://code.google.com/p/episodes/issues/list">bugs</a>
-</nobr>
+<a href="https://github.com/stevesouders/episodes">code</a>
 </div>
 <h1>Web Episodes</h1>
 
 <p style="margin-bottom: 0;">
-<i>Web Episodes</i> is a framework for timing web pages.
-It has three key concepts:
+<i>Web Episodes</i> is a script for gathering Real User Measurement (RUM) performance timings from web pages.
+The key aspects of <i>Web Episodes</i> are:
 <ul>
+  <li> it loads asynchronously
+  <li> it supports <a href="http://www.w3.org/TR/navigation-timing/">Navigation Timing</a> and <a href="http://www.w3.org/TR/resource-timing/">Resource Timing</a>
   <li> it supports measuring Web 2.0 applications
   <li> measurements are made using JavaScript events so there can be multiple listeners
-  <li> its designed to be an industry standard for web developers, web metrics service providers, tool developers, and browser developers
+  <li> it's designed to be an industry standard for web developers, web metrics service providers, tool developers, and browser developers
+  <li> it's open source
 </ul>
 
-<p>
+<p style="margin-bottom: 0;">
 Examples:
 </p>
-<ul>
+<ul style="margin-top: 0;">
   <li> <a href="ex1.html">example 1</a> - a simple example using default settings
 </ul>
 
 <p>
-To illustrate how there can be multiple consumers of Episodes, use the 
-<a href="episodes.xpi" onclick="installXPI('episodes.xpi'); return false;">Episodes Firefox add-on</a> 
-to view episodic timing information from any page that uses Episodes.
+See these <a href="http://ajaxexperience.techtarget.com/conference/html/performance.html#SSoudersFast">slides</a> for more information.
 </p>
 
 </body>
